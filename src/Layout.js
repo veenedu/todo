7f4fc411144d6b4a4e608ee84940557f4cb081ed
Praveen Prasad <veen.edu@gmail.com>
2018-08-09 12:03:57 +0530

    UPdate Layout

--- src/Layout.js
+++ src/Layout.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import TasksApp from './TasksApp';
+import UserManagement from './UserManagement';
 
 class Layout extends Component {
   render() {
     let {user} = this.props;
-    return (
-      <TasksApp />
-    );
+    let Component = !user? UserManagement : TasksApp
+    return <Component />
   }
 }
 
